fe4f97f3cc5f518bd9178c9608f55486c94a2c02
claurav-cyber <claurav@unjbg.edu.pe>
2026-06-23 20:49:49 -0500

    eliminamos el codigo anterior por que avian siertos defectos

--- trabajo5.cpp
+++ trabajo5.cpp
@@ -6,33 +6,30 @@ int main() {
     string line;
     getline(cin, line);
     
-    // Extraer las letras
-    char letras[101];
+    char letra[101];
     int total = 0;
     
     for (int i = 0; i < line.size(); i++) {
         if (line[i] >= 'a' && line[i] <= 'z') {
-            letras[total] = line[i];
+            letra[total] = line[i];
             total++;
         }
     }
     
-    // Ordenar las letras con burbuja
     for (int i = 0; i < total; i++) {
         for (int j = 0; j < total - 1; j++) {
-            if (letras[j] > letras[j + 1]) {
-                char temp = letras[j];
-                letras[j] = letras[j + 1];
-                letras[j + 1] = temp;
+            if (letra[j] > letra[j + 1]) {
+                char temp = letra[j];
+                letra[j] = letra[j + 1];
+                letra[j + 1] = temp;
             }
         }
     }
     
-    // Poner las letras ordenadas de vuelta
     int k = 0;
     for (int i = 0; i < line.size(); i++) {
         if (line[i] >= 'a' && line[i] <= 'z') {
-            line[i] = letras[k];
+            line[i] = letra[k];
             k++;
         }
     }
